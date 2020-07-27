4ea66f31cfc958c838df4e38f27d767e20a37607
kirildobrev919 <kirildobrev919@gmail.com>
2020-07-27 19:29:59 +0100

    Button -/+ added reworked

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Track.css';
 
 export class Track extends React.Component {
-    renderAction (isRemoval) {
-        if(isRemoval){
-            return "-";
+    renderAction () {
+        if(this.props.isRemoval){
+            return <button className="Track-action">-</button>;
         }else{
-            return "+";
+            return <button className="Track-action">+</button>;
         }
     }
 
@@ -14,10 +14,10 @@ export class Track extends React.Component {
         return (
             <div className="Track">
                 <div className="Track-information">
-                    <h3>{/*<!-- track name will go here -->*/}</h3>
-                    <p>{/*<!-- track artist will go here--> | <!-- track album will go here -->*/}</p>
+                    <h3>track name will go here </h3>
+                    <p>track artist will go here  | track album will go here</p>
                 </div>
-                <button className="Track-action">{this.renderAction}</button>
+               {this.renderAction()}
             </div>
         );
     }
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,10 +3,15 @@ import './TrackList.css';
 /*import {Track} from './../Track/Track';*/
 
 export class TrackList extends React.Component {
+
     render() {
         return (
             <div className="TrackList">
-                {/*<!-- You will add a map method that renders a set of Track components  -->*/}
+                {
+                   /* this.props.tracks.map(track => {
+                        return <Track track={track} key={track.id} />
+                    })*/
+                }
             </div>
         );
     }
