b1b9376c4832b68ec9912a3ed5c9da84ff3380a8
kirildobrev919 <kirildobrev919@gmail.com>
2020-09-12 17:06:37 +0100

    Additions to  PlaylistLists js and css

---
+++ src/Components/PlaylistList/PlaylistList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export class PlaylistList extends React.Component {
+
+    constructor(props) {
+        super();
+        this.state = {
+            playlists: []
+        }
+    }
+
+    render() {
+        return
+    }
+}
